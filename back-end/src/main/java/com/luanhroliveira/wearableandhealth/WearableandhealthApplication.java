3c9ed1838f65f59c436b7c584260a4f87b42eb91
luanhroliveira <luanhigor98@gmail.com>
2021-03-02 22:33:07 -0300

    Iniciando projeto Wearable & Health

---
+++ back-end/src/main/java/com/luanhroliveira/wearableandhealth/WearableandhealthApplication.java
@@ -0,0 +1,13 @@
+package com.luanhroliveira.wearableandhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearableandhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearableandhealthApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/luanhroliveira/wearableandhealth/WearableandhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanhroliveira.wearableandhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearableandhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
