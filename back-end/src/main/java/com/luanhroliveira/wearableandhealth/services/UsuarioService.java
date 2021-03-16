5b97d7a11922d078f397744753ad1c933c7443cb
luanhroliveira <luanhigor98@gmail.com>
2021-03-16 19:48:47 -0300

    PUT /usuarios/{id}

--- back-end/src/main/java/com/luanhroliveira/wearableandhealth/services/UsuarioService.java
+++ back-end/src/main/java/com/luanhroliveira/wearableandhealth/services/UsuarioService.java
@@ -45,34 +45,92 @@ public class UsuarioService {
 
 	@Transactional
 	public UsuarioDTO insert(UsuarioDTO dto) {
+		try {
 
-		if (dto.getContatos().size() == 0) {
-			throw new AuthorizationServiceException("Necessário ao mínimo 1(um) contato para o usuário!");
-		} else if (dto.getEnderecos().size() == 0) {
-			throw new AuthorizationServiceException("Necessário ao mínimo 1(um) endereço para o usuário!");
-		} else if (dto.getDataNascimento().after(new Date())) {
-			throw new AuthorizationServiceException("Data de nascimento não pode ser maior que a data atual!");
+			if (dto.getContatos().size() == 0) {
+				throw new AuthorizationServiceException("Necessário ao mínimo 1(um) contato para o usuário!");
+			} else if (dto.getEnderecos().size() == 0) {
+				throw new AuthorizationServiceException("Necessário ao mínimo 1(um) endereço para o usuário!");
+			} else if (dto.getDataNascimento().after(new Date())) {
+				throw new AuthorizationServiceException("Data de nascimento não pode ser maior que a data atual!");
+			}
+
+			Usuario usuario = new Usuario(null, dto.getNome(), dto.getDataNascimento(), dto.getCpf(), Status.ATIVO);
+
+			for (ContatoUsuarioDTO p : dto.getContatos()) {
+				ContatoUsuario contato = new ContatoUsuario(null, usuario, p.getNome(), p.getTelefone(), p.getEmail(),
+						Status.ATIVO);
+				usuario.getContatos().add(contato);
+			}
+
+			for (EnderecoUsuarioDTO p : dto.getEnderecos()) {
+				EnderecoUsuario endereco = new EnderecoUsuario(null, usuario, p.getCidade(), p.getLogradouro(),
+						p.getNumero(), p.getComplemento(), p.getBairro(), p.getCep());
+				usuario.getEnderecos().add(endereco);
+			}
+
+			usuario = usuarioRepository.save(usuario);
+			contatoRepository.saveAll(usuario.getContatos());
+			enderecoRepositpry.saveAll(usuario.getEnderecos());
+
+			return new UsuarioDTO(usuario);
+		} catch (RuntimeException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+	}
+
+	public UsuarioDTO update(Long id, UsuarioDTO dto) {
+
+		try {
+
+			if (dto.getDataNascimento().after(new Date())) {
+				throw new AuthorizationServiceException("Data de nascimento não pode ser maior que a data atual!");
+			}
+
+			Usuario usuario = usuarioRepository.getOne(id);
+			updateData(usuario, dto);
+
+			usuario = usuarioRepository.save(usuario);
+
+			return new UsuarioDTO(usuario);
+
+		} catch (RuntimeException e) {
+			throw new RuntimeException(e.getMessage());
 		}
 
-		Usuario usuario = new Usuario(null, dto.getNome(), dto.getDataNascimento(), dto.getCpf(), Status.ATIVO);
+	}
+
+	private void updateData(Usuario usuario, UsuarioDTO dto) {
+		usuario.setCpf(dto.getCpf());
+		usuario.setDataNascimento(dto.getDataNascimento());
+		usuario.setNome(dto.getNome());
+		usuario.setStatus(dto.getStatus());
 
 		for (ContatoUsuarioDTO p : dto.getContatos()) {
-			ContatoUsuario contato = new ContatoUsuario(null, usuario, p.getNome(), p.getTelefone(), p.getEmail(),
-					Status.ATIVO);
+			ContatoUsuario contato = contatoRepository.getOne(p.getId());
+
+			contato.setEmail(p.getEmail());
+			contato.setNome(p.getNome());
+			contato.setTelefone(p.getTelefone());
+			contato.setStatus(p.getStatus());
+			contato = contatoRepository.save(contato);
+
 			usuario.getContatos().add(contato);
 		}
 
 		for (EnderecoUsuarioDTO p : dto.getEnderecos()) {
-			EnderecoUsuario endereco = new EnderecoUsuario(null, usuario, p.getCidade(), p.getLogradouro(),
-					p.getNumero(), p.getComplemento(), p.getBairro(), p.getCep());
+			EnderecoUsuario endereco = enderecoRepositpry.getOne(p.getId());
+
+			endereco.setCep(p.getCep());
+			endereco.setLogradouro(p.getLogradouro());
+			endereco.setNumero(p.getNumero());
+			endereco.setComplemento(p.getComplemento());
+			endereco.setBairro(p.getBairro());
+			endereco = enderecoRepositpry.save(endereco);
+
 			usuario.getEnderecos().add(endereco);
 		}
 
-		usuario = usuarioRepository.save(usuario);
-		contatoRepository.saveAll(usuario.getContatos());
-		enderecoRepositpry.saveAll(usuario.getEnderecos());
-
-		return new UsuarioDTO(usuario);
 	}
 
 }
